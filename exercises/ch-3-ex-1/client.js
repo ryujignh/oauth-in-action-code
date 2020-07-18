cbf8b206d387532009b4f12215e1768d6a400006
ryujignh <ryujignh@gmail.com>
2020-07-18 20:53:47 +0900

    Implement authorization endpoint

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -45,7 +45,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/authorize', function(req, res){
-	console.log('authorize');
+	access_token = null;
+	state = randomstring.generate()
+
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+
 	res.redirect(authorizeUrl);
 	/*
 	 * Send the user to the authorization server
@@ -57,6 +66,56 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
+	if (req.query.error) {
+		res.render('error', {error: req.query.error});
+		return;
+	}
+
+	if (req.query.state != state) {
+		console.log('State DOES NOT MATCH: expected %s got %s', state, req.query.state);
+		res.render('error', {error: 'State value did not match'});
+		return;
+	}
+
+	var code = req.query.code;
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(
+			client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+			body: form_data,
+			headers: headers
+		});
+
+	console.log('Requesting access token for code %s', code);
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		var body = JSON.parse(tokRes.getBody());
+		access_token = body.access_token;
+
+		console.log('got access token: %s', access_token);
+
+		res.render('index', {
+			access_token: access_token,
+			scope: scope
+		});
+	} else {
+		res.render('error', {
+			error: 'Unable to fetch access token, serverresponse:' +
+				tokRes.statusCode
+		});
+	}
+
+
 
 });
 
@@ -83,13 +142,6 @@ var buildUrl = function(base, options, hash) {
 
 	return url.format(newUrl);
 };
-
-var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
-	response_type: 'code',
-	client_id: client.client_id,
-	redirect_uri: client.redirect_uris[0]
-});
-
 
 var encodeClientCredentials = function(clientId, clientSecret) {
 	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
